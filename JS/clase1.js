189f14c35160aa1f87b251d2d5ce98777242f97e
AngelSilva98 <angelceesd@gmail.com>
2022-11-28 11:43:51 -0300

    inicio git

---
+++ JS/clase1.js
@@ -0,0 +1,46 @@
+// 1
+
+console.log("¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!");
+
+//2
+
+const numero1 = 12
+
+//3
+
+const numero2 = 33
+
+//4
+
+console.log(numero1 + numero2);
+
+//5
+
+// let nombreUsuario = prompt("Ingrese su nombre completo");
+
+//6
+
+console.log("Bienvenide a nuestra primera web", nombreUsuario);
+
+//7
+
+let numero3 = (5*numero2);
+console.log(numero3);
+
+//8
+
+const nombre1 = "Carlos"
+const nombre2 = "Juan"
+const nombre3 = "Pedro"
+
+console.log(nombre1,nombre2,nombre3);
+
+//9
+
+let numero4 = numero3-100;
+console.log(numero4);
+
+//10
+
+console.log(typeof numero2);
+console.log(typeof nombreUsuario);
